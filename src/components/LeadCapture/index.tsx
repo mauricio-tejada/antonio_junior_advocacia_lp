229a8fe83e6d85b28192078a545ea59f7f566c39
Mauricio <you@example.com>
2023-01-26 22:43:05 -0300

    form rollback

--- src/components/LeadCapture/index.tsx
+++ src/components/LeadCapture/index.tsx
@@ -8,37 +8,31 @@ import {
   LeadCaptureContainer,
   TextArea,
 } from './style'
-// import { useForm } from 'react-hook-form'
-// import { zodResolver } from '@hookform/resolvers/zod'
-// import * as zod from 'zod'
-// import { useState } from 'react'
-
-import { useForm as useFormspree, ValidationError } from '@formspree/react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useState } from 'react'
 
 export function LeadCapture() {
-  // const leadCaptureFormValitationSchema = zod.object({
-  //   primeiro_nome: zod.string().min(1, 'Favor preencher'),
-  //   ultimo_nome: zod.string().min(1, 'Favor preencher'),
-  //   email: zod.string().min(1, 'Favor preencher'),
-  //   telefone: zod.string().min(1, 'Favor preencher'),
-  //   caso: zod.string().min(1, 'Favor preencher'),
-  // })
+  const leadCaptureFormValitationSchema = zod.object({
+    primeiro_nome: zod.string().min(1, 'Favor preencher'),
+    ultimo_nome: zod.string().min(1, 'Favor preencher'),
+    email: zod.string().min(1, 'Favor preencher'),
+    telefone: zod.string().min(1, 'Favor preencher'),
+    caso: zod.string().min(1, 'Favor preencher'),
+  })
 
-  // type leadCaptureFormData = zod.infer<typeof leadCaptureFormValitationSchema>
-  // // const [formData, setFormData] = useState<leadCaptureFormData>()
+  type leadCaptureFormData = zod.infer<typeof leadCaptureFormValitationSchema>
+  const [formData, setFormData] = useState<leadCaptureFormData>()
 
-  // const { register, reset, formState } = useForm<leadCaptureFormData>({
-  //   resolver: zodResolver(leadCaptureFormValitationSchema),
-  // })
+  const { register, reset, handleSubmit, formState } =
+    useForm<leadCaptureFormData>({
+      resolver: zodResolver(leadCaptureFormValitationSchema),
+    })
 
-  // function handleCreateNewSubmit(data: leadCaptureFormData) {
-  //   setFormData(data)
-  //   reset()
-  // }
-
-  const [state, handleSubmit] = useFormspree('mzbqdzjv')
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>
+  function handleCreateNewSubmit(data: leadCaptureFormData) {
+    setFormData(data)
+    reset()
   }
 
   return (
@@ -52,66 +46,59 @@ export function LeadCapture() {
               equipe avaliar, entraremos em contato!
             </p>
           </header>
-          <form onSubmit={handleSubmit} action="">
+          <form onSubmit={handleSubmit(handleCreateNewSubmit)} action="">
             <FormRow>
               <InputContainer>
-                <Input placeholder="Primeiro nome" />
-                <ValidationError
-                  prefix="Primeiro nome"
-                  field="Primeiro nome"
-                  errors={state.errors}
+                <Input
+                  {...register('primeiro_nome')}
+                  placeholder="Primeiro nome"
+                  onEmpty={!!formState.errors.primeiro_nome}
                 />
-                {/* <p>{formState.errors.primeiro_nome?.message}</p> */}
+                <p>{formState.errors.primeiro_nome?.message}</p>
               </InputContainer>
 
               <InputContainer>
-                <Input placeholder="Último nome" />
-                <ValidationError
-                  prefix="Último nome"
-                  field="Último nome"
-                  errors={state.errors}
+                <Input
+                  {...register('ultimo_nome')}
+                  placeholder="Último nome"
+                  onEmpty={!!formState.errors.ultimo_nome}
                 />
-                {/* <p>{formState.errors.ultimo_nome?.message}</p> */}
+                <p>{formState.errors.ultimo_nome?.message}</p>
               </InputContainer>
             </FormRow>
 
             <FormRow>
               <InputContainer>
-                <Input placeholder="E-mail" />
-                <ValidationError
-                  prefix="Email"
-                  field="Email"
-                  errors={state.errors}
+                <Input
+                  {...register('email')}
+                  placeholder="E-mail"
+                  onEmpty={!!formState.errors.email}
                 />
-                {/* <p>{formState.errors.email?.message}</p> */}
+                <p>{formState.errors.email?.message}</p>
               </InputContainer>
 
               <InputContainer>
-                <Input placeholder="Número de telefone" />
-                <ValidationError
-                  prefix="Número de telefone"
-                  field="Número de telefone"
-                  errors={state.errors}
+                <Input
+                  {...register('telefone')}
+                  placeholder="Número de telefone"
+                  onEmpty={!!formState.errors.telefone}
                 />
-                {/* <p>{formState.errors.telefone?.message}</p> */}
+                <p>{formState.errors.telefone?.message}</p>
               </InputContainer>
             </FormRow>
 
             <InputContainer>
               <TextArea
+                {...register('caso')}
                 cols={25}
                 rows={10}
                 placeholder="Conte um pouco sobre o seu caso..."
+                onEmpty={!!formState.errors.caso}
               />
-              <ValidationError
-                prefix="caso"
-                field="caso"
-                errors={state.errors}
-              />
-              {/* <p>{formState.errors.caso?.message}</p> */}
+              <p>{formState.errors.caso?.message}</p>
             </InputContainer>
 
-            <DefaultButton type="submit" disabled={state.submitting}>
+            <DefaultButton type="submit">
               Enviar consulta gratuita
             </DefaultButton>
           </form>
